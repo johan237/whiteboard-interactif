a59e3b5044ceaa3384c60e46f602204b79fe6287
johan237 <johantchassem@gmail.com>
2024-01-28 22:13:38 -0500

    first

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app)
+
+const { v4: uuidV4 } = require('uuid')
+
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId)
+        socket.to(roomId).emit('user-connected', userId)
+
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId)
+        })
+    })
+})
+
+server.listen(3000, () => {
+    console.log('running on port 3000')
+})
