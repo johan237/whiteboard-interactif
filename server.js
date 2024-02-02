ab56be9c64853a1c4a9abda98a6be696635febe2
Johan Kemmegne Tchassem <kemj1301@usherbrooke.ca>
2024-02-02 15:10:38 -0500

    some changes

--- server.js
+++ server.js
@@ -14,22 +14,60 @@ app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
+app.get('/home', (req, res) => {
+    res.render('index')
 })
 
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).emit('user-connected', userId)
+
+// app.get('/:room', (req, res) => {
+//     res.render('room', { roomId: req.params.room })
+// })
+
+// io.on('connection', socket => {
+//     console.log('Connected')
+//     socket.on('join-room', (roomId, userId) => {
+//         socket.join(roomId)
+//         socket.to(roomId).emit('user-connected', userId)
 
 
-        socket.on('disconnect', () => {
-            socket.to(roomId).emit('user-disconnected', userId)
-        })
+//         socket.on('disconnect', () => {
+//             socket.to(roomId).emit('user-disconnected', userId)
+//         })
+//     })
+
+
+// })
+
+io.on('connection', (socket) => {
+    console.log('A user connected with socket : ', socket.id);
+
+    // Handle message event
+
+    socket.on('user-drawing', data => {
+        console.log(data)
+        console.log('User is drawing')
+        socket.broadcast.emit('user-drawing', data)
     })
-})
+
+    socket.on('initialising', data => {
+        socket.broadcast.emit('initialising', data)
+    })
+
+
+    socket.on('user-not-drawing', () => {
+        socket.broadcast.emit('user-not-drawing')
+    })
+
+    socket.on('change-color', (color) => {
+        socket.broadcast.emit('change-color', color)
+    })
+
+    // Handle disconnect event
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
 
 server.listen(3000, () => {
     console.log('running on port 3000')
